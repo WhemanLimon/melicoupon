32d21335658406c94632aabf66809e05b7d2f397
Wheman <spam@wheman.net>
2021-04-07 21:45:09 -0300

    init repo

---
+++ src/main/java/net/wheman/melicoupon/MelicouponApplication.java
@@ -0,0 +1,13 @@
+package net.wheman.melicoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelicouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelicouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/wheman/melicoupon/MelicouponApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wheman.melicoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelicouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
